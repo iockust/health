66f567e6b0561d3898f0c95b4b02284f197e8de2
mr-haseeb <bolahaseeb@gmail.com>
2020-07-06 20:03:07 +0500

    change patientlist to healthPatient

--- dashborad/api/filters.py
+++ dashborad/api/filters.py
@@ -3,6 +3,7 @@ import django_filters
 
 
 class HealthFilter(django_filters.FilterSet):
+     time = django_filters.CharFilter(name='time',lookup_expr='iexact')
     class Meta:
         model = Health
         fields = ['time', 'value']
