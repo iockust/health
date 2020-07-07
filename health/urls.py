1fa0c0fd0c8b943c83e9362707d9611f7f2531c8
mr-haseeb <bolahaseeb@gmail.com>
2020-07-07 23:05:10 +0500

    added api url

--- health/urls.py
+++ health/urls.py
@@ -45,8 +45,8 @@ urlpatterns = [
 
     # Documentation of Api
     
-   url('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   url('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 
