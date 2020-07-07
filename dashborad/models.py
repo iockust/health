468abf6d2dff167aee33fdd6ba4fe3e1aba3488f
mr-haseeb <bolahaseeb@gmail.com>
2020-07-07 18:56:19 +0500

    add model

--- dashborad/models.py
+++ dashborad/models.py
@@ -91,7 +91,7 @@ class WeeklyHealthSummaryManager(models.Manager):
         from django.db import connection
         with connection.cursor() as cursor:
 
-            cursor.execute(""" SELECT avg(value) HeartRate, AVG(Intensity) Intensity,AVG(SleepMinute) AS Sleep,Min(value) as MinHeartReate,Max(value) as MaxHearRate
+            cursor.execute(""" SELECT avg(value) HeartRate, AVG(Intensity) Intensity,AVG(SleepMinute) AS Sleep,AVG(SleepMinute),Min(value) as MinHeartReate,Max(value) as MaxHearRate
             FROM `dashborad_health`  where Id=%s and (Time BETWEEN %s AND %s ) """,(pid, startdate, enddate))
             result_list = []
             for row in cursor.fetchall():
