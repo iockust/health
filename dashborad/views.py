6b55228b2b43952488c478ec36ad67fd7337f2f9
mr-haseeb <bolahaseeb@gmail.com>
2020-07-08 14:15:36 +0500

    remove error

--- dashborad/views.py
+++ dashborad/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import JsonResponse
-from dashoborad.models import Health, Patient
+from . models import Health, Patient
 from django.core import serializers
 
 
